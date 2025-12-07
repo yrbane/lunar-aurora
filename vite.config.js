4aa7f4b325b1efd86b195a32815d958bb894a39c
Seb <seb@example.com>
2025-12-07 13:01:28 +0100

    chore(vite): update dev server port to 3001

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 export default defineConfig({
   root: 'examples', // Racine du serveur de dev
   server: {
-    port: 3000,
+    port: 3001,
     open: true
   },
   resolve: {
